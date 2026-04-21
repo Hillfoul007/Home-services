275a98402da649d4a920e834b68b8aa05e222815
Chaman Kataria <sunflower110001@gmail.com>
2026-04-21 17:02:55 +0530

    Rider ETA and gps

--- scripts/build-desk-aab.sh
+++ scripts/build-desk-aab.sh
@@ -9,8 +9,8 @@ APP_FLAVOR="desk"
 DIST_DIR="dist-${APP_FLAVOR}"
 APP_ID="com.laundrify.desk.app"
 APP_NAME="Laundrify Desk"
-VERSION_CODE=64
-VERSION_NAME="2.64"
+VERSION_CODE=66
+VERSION_NAME="2.66"
 DEFAULT_APP_ID="com.laundrify.laundry.app"
 DEFAULT_APP_NAME="Laundrify"
 
--- scripts/build-user-aab.sh
+++ scripts/build-user-aab.sh
@@ -7,8 +7,8 @@ set -e
 KEYSTORE_FILE="laundrify-release-keystore.jks"
 APP_ID="com.laundrify.laundry.app"
 APP_NAME="Laundrify"
-VERSION_CODE=64
-VERSION_NAME="2.64"
+VERSION_CODE=66
+VERSION_NAME="2.66"
 
 echo "📱 Building User App Bundle (AAB)..."
 echo ""
